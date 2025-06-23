e8946c677ae035ae7d5f66f397cf1286107fe178
Luis-60 <luiscarlosghfc@gmail.com>
2025-06-23 19:01:42 -0300

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import Navbar from "@/scenes/navbar/";
+import { useState } from "react";
+
+type SelectedPage = "home" | "benefits" | "ourclasses" | "contactus";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<
+  SelectedPage>("home");
 
   return (
-    <>
-      <div className="app">
-
-      </div>
-              </>
-  )
+    <div className="app bg-gray-200">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
 
 export default App
