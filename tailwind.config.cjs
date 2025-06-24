ab71d328455038e26a7d559731c3d7f860bf62c2
Luis-60 <luiscarlosghfc@gmail.com>
2025-06-24 10:11:39 -0300

    navbar finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
         "gray-20": "#F8F4EB",
         "gray-50": "#EFE6E6",
         "gray-100": "#DFCCCC",
-        "brand-gray-500": "#5E0000",
+        "gray-500": "#5E0000",
         "primary-100": "#FFE1E0",
         "primary-300": "#FFA6A3",
         "primary-500": "#FF6B66",
